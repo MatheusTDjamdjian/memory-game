a4d353466e29e05114b8e615678c83c113076162
Matheus <matheustassodjamdjian@gmail.com>
2024-11-11 16:29:01 -0300

    click event changing

--- matching-game/src/components/CardFrontBack/index.js
+++ matching-game/src/components/CardFrontBack/index.js
@@ -2,8 +2,19 @@ import CardGame from "../CardGame"
 import "./style.css"
 
 function CardFrontBack() {
+    window.cardFrontBack = {}
+    window.cardFrontBack.handleClick = (event) => {
+        const $origin = event.target
+        const $CardFrontBack = $origin.parentNode.parentNode.parentNode
+
+        console.log($origin)
+        console.log($CardFrontBack)
+
+        $CardFrontBack.classList.add('-active')
+    }
+
     return /* html */`
-      <article class="card_front_back">
+      <article class="card_front_back" onClick="cardFrontBack.handleClick(event)">
         <div class="card -front">
             ${CardGame()}
         </div>
