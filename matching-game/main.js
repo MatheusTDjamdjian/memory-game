931630f66c2519a877855ba7af7cfdf50a1a3a29
Matheus <matheustassodjamdjian@gmail.com>
2024-11-11 11:49:16 -0300

    create playername component

--- matching-game/main.js
+++ matching-game/main.js
@@ -1,11 +1,17 @@
 import './src/styles/generic/reset.css'
 
 import BoardGame from './src/objects/BoardGame';
+import PlayerName from './src/components/PlayerName';
 
 import './src/styles/settings/colors.css';
 import './src/styles/elements/base.css'
 
 const $root = document.querySelector("#root");
-const $htmlBoardGame = BoardGame(6);
 
-$root.insertAdjacentHTML("beforeend", $htmlBoardGame);
+$root.insertAdjacentHTML("beforeend", 
+    ` 
+    ${PlayerName("Player1")}
+    ${PlayerName("Player2 ")}
+    ${BoardGame(6)} 
+    `
+);
