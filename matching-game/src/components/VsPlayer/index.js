a88d35fd769606a85d33337acb887ab7708a060a
Matheus <matheustassodjamdjian@gmail.com>
2024-11-22 14:33:09 -0300

    creating vsplayer

---
+++ matching-game/src/components/VsPlayer/index.js
@@ -0,0 +1,7 @@
+function VsPlayer(content = "vs") {
+    return /* html */ `
+      <span class="vs_player">${content}</span>
+    `
+}
+
+export default VsPlayer
--- matching-game/src/objects/ScoreBoard/index.js
+++ matching-game/src/objects/ScoreBoard/index.js
@@ -1,10 +1,12 @@
 import PlayerName from "../../components/PlayerName"
+import VsPlayer from "../../components/VsPlayer";
 import "./style.css"
 
 function ScoreBoard() {
     return /* html */`
       <header class="score_board">
         ${PlayerName("Player1")}
+        ${VsPlayer()}
         ${PlayerName("Player2 ")}
       </header>
     `;
