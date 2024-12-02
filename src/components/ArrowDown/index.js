b5e8dd4e89ee9d1248bdf55cc761486f12439188
Matheus <matheustassodjamdjian@gmail.com>
2024-12-02 10:39:43 -0300

    adding changes

--- src/components/ArrowDown/index.js
+++ src/components/ArrowDown/index.js
@@ -2,7 +2,7 @@ import "./style.css"
 
 function ArrowDown(currentPlayer = 1) {
     return /* html */ `
-      <img class="arrow_down" data-currentPlayer= "${currentPlayer}" src="src/img/IconArrowDown.png" alt="ícone de uma seta para baixo">
+      <img class="arrow_down" data-currentPlayer= "${currentPlayer}" src="./src/img/IconArrowDown.png" alt="ícone de uma seta para baixo">
     `
 } 
 
