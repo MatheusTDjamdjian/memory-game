e28add486bab9f4fb4f463b889bdbf0b712ef21e
Matheus <matheustassodjamdjian@gmail.com>
2024-12-02 11:03:35 -0300

    changing image path

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -3,7 +3,7 @@ import './style.css'
 function CardGame (icon = "cara-feliz", alt = "Cara Feliz") {
     return /* html */`
     <article class="card_game">
-      <img src="./src/img/${icon}.png" alt="${alt}">
+      <img src="/src/img/${icon}.png" alt="${alt}">
     </article>
     `
 }
